8d8caaec217abbdb3649d9bb48cf92a2e9ffe7fa
itsloraee <laurxee@gmail.com>
2025-11-26 19:56:59 -0400

    Création du dépot GIT

---
+++ database/migrations/2025_11_26_132045_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('user')->onDelete('cascade');
+            $table->float('prix',8,2);
+            $table->string('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
